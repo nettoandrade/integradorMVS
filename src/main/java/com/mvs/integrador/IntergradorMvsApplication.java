e4e877dc2f578c5fd9213f2d1edcd89ba8f99daa
Antonio Andrade <antonio_netu@hotmail.com>
2019-08-27 00:24:40 -0300

    Projeto Spring Boot

---
+++ src/main/java/com/mvs/integrador/IntergradorMvsApplication.java
@@ -0,0 +1,13 @@
+package com.mvs.integrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntergradorMvsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntergradorMvsApplication.class, args);
+	}
+
+}
